dc1a8e7b5f803e04d5a6be9434a46bd73b497805
Cyrus Spencer <cyrus.spencer119@gmail.com>
2022-05-05 10:33:49 -0400

    More Commit

---
+++ Arsenal.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Arsenal.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("Arsenal.Tests")]
+[assembly: AssemblyCopyright("Copyright © HP 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("282bfc0b-d067-450c-a212-621c346fee68")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ RedBadgeFinal/Controllers/StadiumController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RedBadgeFinal.Controllers
+{
+    public class StadiumController : Controller
+    {
+        // GET: Stadium
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
